4b59ecfc42e58e11dfed3d512463668d26ee3595
ArsLoD <arslod123@gmail.com>
2024-02-18 19:05:55 +0200

    upd

--- HW_3/index.ts
+++ HW_3/index.ts
@@ -1,30 +1,66 @@
 import fs from 'fs'
 import fsPromise from 'fs/promises'
 import path from 'path'
-
+import { v4 as uuidv4 } from 'uuid'
 type Schema = Record<string, any>
+enum DataTypes {
+	Number = 'number',
+	String = 'string',
+	Date = 'date',
+}
 
 class FileDB {
 	path: string
 	schemas: Record<string, Schema> = {}
+	currentTables: Record<string, TableDB> = {}
 
 	constructor(path: string) {
 		if (!fs.existsSync(path)) {
 			fs.mkdirSync(path)
 		}
 		this.path = path
+		this.currentTables = {}
 	}
+	async initFiles(): Promise<Record<string, TableDB>> {
+		const files = await fsPromise.readdir(this.path)
+		for (const file of files) {
+			const data = await fsPromise.readFile(path.join(this.path, file), 'utf-8')
+			const parsedData = JSON.parse(data)
+			const tableName = file.split('.')[0]
+			const newTable = new TableDB(
+				tableName,
+				parsedData[tableName],
+				parsedData.schema
+			)
+			this.currentTables[tableName] = newTable
+			this.schemas[tableName] = parsedData.schema
+		}
+		return this.currentTables
+	}
+
 	async registerSchema(tableName: string, schema: Schema): Promise<void> {
 		if (
 			!this.schemas[tableName] &&
 			!fs.existsSync(this.path + `/${tableName}.json`)
 		) {
 			try {
+				const newSchema: Schema = Object.assign({}, schema)
+				for (const key in newSchema) {
+					if (newSchema[key] === Number) {
+						newSchema[key] = DataTypes.Number
+					} else if (newSchema[key] === String) {
+						newSchema[key] = DataTypes.String
+					} else if (newSchema[key] === Date) {
+						newSchema[key] = DataTypes.Date
+					}
+				}
 				await fsPromise.writeFile(
 					this.path + `/${tableName}.json`,
-					JSON.stringify({ [tableName]: [] })
+					JSON.stringify({
+						[tableName]: [],
+						schema: Object.assign({}, newSchema),
+					})
 				)
-
 				console.log(`Table ${tableName} created`)
 			} catch (error) {
 				console.error(error)
@@ -33,21 +69,17 @@ class FileDB {
 			console.log('Table already exists')
 		}
 		this.schemas[tableName] = schema
-		// this.getSchemas()
 	}
 
 	getSchemas(): Record<string, Schema> {
-		// console.log(this.schemas)
 		return this.schemas
 	}
 
 	async getTable(tableName: string): Promise<TableDB> {
-		const data = await fsPromise
-			.readFile(this.path + `/${tableName}.json`, 'utf-8')
-			.then(data => JSON.parse(data))
-			.catch(error => console.error(error))
-
-		return new TableDB(tableName, data, this.schemas[tableName])
+		if (this.currentTables[tableName]) {
+			return this.currentTables[tableName]
+		}
+		throw new Error(`Table ${tableName} does not exist`)
 	}
 }
 
@@ -65,20 +97,12 @@ class TableDB {
 		this.schema = schema
 	}
 	//C
-	async create(newData: Schema): Promise<void> {
-		const fileData = await fsPromise
-			.readFile(
-				path.join(__dirname + '/DB/FileDB', `${this.tableName}.json`),
-				'utf-8'
-			)
-			.catch(error => {
-				console.error(`Error reading file ${this.tableName}.json:`, error)
-				return null
-			})
+	async create(newData: Schema): Promise<void | Schema> {
+		const fileData = await this.getFileData()
 
+		// console.log('fileData', fileData)
 		if (fileData) {
-			const parsedData = JSON.parse(fileData)
-			const existingItem = parsedData[this.tableName].find(
+			const existingItem = fileData[this.tableName].find(
 				(item: any) => item.id === newData.id
 			)
 			if (existingItem) {
@@ -91,19 +115,26 @@ class TableDB {
 		if (!this.isValidData(newData)) {
 			return
 		}
-
-		this.data[this.tableName].push(newData)
-		this.writeFile(this.data[this.tableName])
+		const lastData = Object.assign(
+			{
+				id: parseInt(uuidv4().replace(/\D/g, '').substring(0, 10)),
+				createDate: new Date(),
+			},
+			newData
+		)
+		this.data.push(lastData)
+		this.writeFile(this.data)
+		return lastData
 	}
 	//R
-	getById(id: number | string): Schema {
+	getById(id: number): Schema {
 		return this.data[this.tableName].find((item: any) => item.id === id)
 	}
 	getAll(): Schema[] {
 		return this.data[this.tableName]
 	}
 	//U
-	async update(id: number | string, newData: Schema): Promise<void> {
+	async update(id: number, newData: Schema): Promise<void | Schema> {
 		const parsedData = await this.getFileData()
 		if (!parsedData[this.tableName].find((item: any) => item.id === id)) {
 			console.log(
@@ -114,16 +145,24 @@ class TableDB {
 		if (!this.isValidData(newData)) {
 			return
 		}
+		let updatedObj: Schema = {}
 		const newDataArr = parsedData[this.tableName].map((item: any) => {
 			if (item.id === id) {
-				return { ...item, ...newData }
+				updatedObj = {
+					...item,
+					...newData,
+					id: item.id,
+					createDate: item.createDate,
+				}
+				return updatedObj
 			}
 			return item
 		})
 		this.writeFile(newDataArr)
+		return updatedObj
 	}
 	//D
-	async delete(id: number | string): Promise<void> {
+	async delete(id: number): Promise<void | number> {
 		const parsedData = await this.getFileData()
 		if (!parsedData[this.tableName].find((item: any) => item.id === id)) {
 			console.log(
@@ -135,6 +174,7 @@ class TableDB {
 			(item: any) => item.id !== id
 		)
 		this.writeFile(newData)
+		return id
 	}
 
 	//Other
@@ -157,11 +197,11 @@ class TableDB {
 		}
 		return isValid
 	}
-	async writeFile(data: Schema[]): Promise<void> {
+	async writeFile(data: Schema | Schema[]): Promise<void> {
 		await fsPromise
 			.writeFile(
 				path.join(__dirname + '/DB/FileDB', `${this.tableName}.json`),
-				JSON.stringify({ [this.tableName]: data })
+				JSON.stringify({ [this.tableName]: data, schema: this.schema })
 			)
 			.catch(error => {
 				console.error(
@@ -172,7 +212,6 @@ class TableDB {
 	}
 }
 
-const fileDB = new FileDB(path.join(__dirname + '/DB', 'FileDB'))
 const newspostSchema = {
 	id: Number,
 	title: String,
@@ -181,11 +220,23 @@ const newspostSchema = {
 }
 const newDaySchema = {
 	id: Number,
-	date: Date,
+	createDate: Date,
 }
-fileDB.registerSchema('newspost', newspostSchema)
-fileDB.registerSchema('newDay', newDaySchema)
+
 ;(async () => {
+	const fileDB = new FileDB(path.join(__dirname + '/DB', 'FileDB'))
+	// await fileDB.registerSchema('newspost', newspostSchema)
+	// await fileDB.registerSchema('newDay', newDaySchema)
+	await fileDB.initFiles()
+	// console.log(await fileDB.initFiles())
 	const newspostTable = await fileDB.getTable('newspost')
-	const newDayTable = await fileDB.getTable('newDay')
+	// console.log(newspostTable)
+	console.log(
+		await newspostTable.create({
+			title: 'asasdwqe123123',
+			text: 'Text',
+		})
+	)
+
+	// const newDayTable = await fileDB.getTable('newDay')
 })()
